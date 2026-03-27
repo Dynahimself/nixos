4fdcb8c6953a12291b8adbd0060e4e430acdf0f2
Dynahimself <Dynasti.video@gmail.com>
2026-03-26 21:40:02 -0400

    bleh

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -21,5 +21,8 @@
     # Driver version
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-  environment.etc."hypr/monitor.conf".text = "monitor = , preferred, auto, 1  ";
+  environment.etc."hypr/monitor.conf".text = ''
+    monitor = DP-1, 1920x1080@144, 0x0, 1
+    monitor = HDMI-A-1, 1920x1080@75, 1920x0, 1
+  '';
 }
