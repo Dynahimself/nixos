22fdc3edd98d7fe72ce0d38b0d6f99d8130c8923
Dynahimself <Dynasti.video@gmail.com>
2026-03-29 18:51:48 -0400

    latest BACKUP HARDWARE

--- home.nix
+++ home.nix
@@ -72,6 +72,17 @@ in
     signing.format = null;
   };
 
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+      };
+    };
+  };
+
   home.pointerCursor = {
     gtk.enable = true;
     package = pkgs.catppuccin-cursors.mochaDark;
