105eadd2f4706f41c4fd093946468a405961ff5b
Dynahimself <Dynasti.video@gmail.com>
2026-03-30 12:27:32 -0400

    bleh

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -107,5 +107,8 @@ require("conform").setup({
   },
 })
 vim.g.dbs = {
-  { name = "class_oracle", url = "oracle://C##FOURNIEO:Lycia@205.237.244.252:1521/ORCL" },
+  {
+    name = "Oracle Class",
+    url = "oracle://C##FOURNIEO:Lycia@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=205.237.244.252)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCL)))",
+  },
 }
